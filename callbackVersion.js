a6921979ccee7670693721647fe76593ba7dbf0a
Shawn Lawson <shawnclawson@hotmail.com>
2026-06-01 02:34:16 -0400

    summer chores completed

---
+++ callbackVersion.js
@@ -0,0 +1,54 @@
+function mowYard(personName, callback) {
+    console.log(`${personName} is starting to mow the yard...`);
+    setTimeout(() => {
+        console.log(`${personName.toUpperCase()} mowed the yard.\n`);
+        callback(); 
+    }, 2000);
+}
+
+function weedEatYard(personName, callback) {
+    console.log(`${personName} is starting to weedeat the yard...`);
+    setTimeout(() => {
+        console.log(`${personName.toUpperCase()} weedeated the yard.\n`);
+        callback(); 
+    }, 1500);
+}
+
+function trimHedges(personName, callback) {
+    console.log(`${personName} is started to trim the Hedges...`);
+    setTimeout(() => {
+        console.log(`${personName.toUpperCase()} trimed the Hedges.\n`);
+        callback(); 
+    }, 1000);
+}
+
+
+function collectWood(personName, callback) {
+    console.log(`${personName} is going to collect wood...`);
+    setTimeout(() => {
+        console.log(`${personName.toUpperCase()} collected the wood.\n`);
+        callback(); 
+    }, 1800);
+}
+
+
+function waterGarden(personName, callback) {
+    console.log(`${personName} is starting to water the garden...`);
+    setTimeout(() => {
+        console.log(`${personName.toUpperCase()} watered the garden.\n`);
+        callback(); 
+    }, 1200);
+}
+
+
+mowYard("Alex", () => {
+    weedEatYard("Alex", () => {
+        trimHedges("Alex", () => {
+            collectWood("Alex", () => {
+                waterGarden("Alex", () => {
+                    console.log("🌟 Whew! Alex is finally done with all the chores!");
+                });
+            });
+        });
+    });
+});
